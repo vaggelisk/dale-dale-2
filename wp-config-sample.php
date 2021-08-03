a22febbe8cc320d4442c1845d339c0ed190bcb6f
Vagelis Karakaxis <vkarakaxis@obor-sinobridge.com>
2021-08-03 19:07:45 +0300

    sec reasons

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dale_dale__vue' );
+define( 'DB_NAME', '' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', '' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'koskina' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
